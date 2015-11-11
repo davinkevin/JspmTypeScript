11c018b93e9c316c30b0c3d0ec4c21cff31e7fe3
Davin Kevin <davin.kevin@gmail.com>
2015-11-11 10:02:57 +0100

    feat(module): support multi module import | export

---
+++ public/another-module.ts
@@ -0,0 +1,7 @@
+/**
+    * Created by kevin on 11/11/2015 for JspmTypeScript
+    */
+
+export default {
+    name : 'module'
+}
--- public/app.ts
+++ public/app.ts
@@ -1,6 +1,9 @@
 /**
-    * Created by kevin on 11/11/2015 for JspmTypeScript
-    */
+ * Created by kevin on 11/11/2015 for JspmTypeScript
+ */
+import module from './another-module';
 
 let value : string = "Foo";
+
 console.log(value);
+console.log(module.name);
